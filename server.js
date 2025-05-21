14179e5cdb004acbb053d690ff4ab30b75f5afee
jc <jcreyes078@gmail.com>
2025-05-22 01:59:05 +0800

    container

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' } // allow all origins, change for production
+  cors: { origin: '*' }
 });
 
 const port = process.env.PORT || 3000;
 
 const roomsUsers = {};  // { roomCode: { socketId: username, ... } }
+const waitingQueue = []; // { socket, username }
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // Store username for this socket
   let currentRoom = null;
   let username = null;
 
@@ -25,11 +25,9 @@ io.on('connection', (socket) => {
 
   socket.on('joinRoom', (room) => {
     if (currentRoom) {
-      // Leave old room
       socket.leave(currentRoom);
       if (roomsUsers[currentRoom]) {
         delete roomsUsers[currentRoom][socket.id];
-        // Update user count to room
         io.to(currentRoom).emit('activeUsers', Object.keys(roomsUsers[currentRoom]).length);
       }
     }
@@ -40,33 +38,71 @@ io.on('connection', (socket) => {
     if (!roomsUsers[room]) roomsUsers[room] = {};
     roomsUsers[room][socket.id] = username || 'Anonymous';
 
-    // Send updated active user count to the room
     io.to(room).emit('activeUsers', Object.keys(roomsUsers[room]).length);
 
     console.log(`${username || 'User'} joined room ${room}`);
   });
 
   socket.on('draw', (data) => {
-    // Broadcast drawing data to others in the room except sender
-    if (currentRoom)
-      socket.to(currentRoom).emit('draw', data);
+    if (currentRoom) socket.to(currentRoom).emit('draw', data);
   });
 
   socket.on('chat', (data) => {
-    if (currentRoom) {
-      // Broadcast chat message to all in the room, including sender (sender can ignore if desired)
-      io.to(currentRoom).emit('chat', data);
+    if (currentRoom) io.to(currentRoom).emit('chat', data);
+  });
+
+  // 🚀 Matchmaking logic
+  socket.on('findPartner', () => {
+    if (!username) {
+      socket.emit('error', 'Username not set');
+      return;
+    }
+
+    const partner = waitingQueue.find(user => user.socket.id !== socket.id);
+
+    if (partner) {
+      waitingQueue.splice(waitingQueue.indexOf(partner), 1);
+
+      const room = generateRoomCode();
+
+      [partner.socket, socket].forEach(s => {
+        s.join(room);
+        s.emit('partnerFound', { room, partner: s === socket ? partner.username : username });
+      });
+
+      currentRoom = room;
+      if (!roomsUsers[room]) roomsUsers[room] = {};
+      roomsUsers[room][socket.id] = username;
+      roomsUsers[room][partner.socket.id] = partner.username;
+
+      io.to(room).emit('activeUsers', Object.keys(roomsUsers[room]).length);
+      console.log(`${username} matched with ${partner.username} in room ${room}`);
+    } else {
+      waitingQueue.push({ socket, username });
+      socket.emit('waiting', 'Waiting for a partner...');
+      console.log(`${username} is waiting for a match`);
     }
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
 
+    // Remove from waiting queue
+    const index = waitingQueue.findIndex(u => u.socket.id === socket.id);
+    if (index !== -1) waitingQueue.splice(index, 1);
+
     if (currentRoom && roomsUsers[currentRoom]) {
       delete roomsUsers[currentRoom][socket.id];
       io.to(currentRoom).emit('activeUsers', Object.keys(roomsUsers[currentRoom]).length);
     }
   });
+
+  function generateRoomCode() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let code = '';
+    for (let i = 0; i < 6; i++) code += chars[Math.floor(Math.random() * chars.length)];
+    return code;
+  }
 });
 
 server.listen(port, () => {
